0f332d8c8c9ec0c9079cb8e49622b71aaeeb0acb
omarjafor <omarjaforchy@gmail.com>
2023-10-05 22:36:01 +0600

    image json file updated

--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -6,7 +6,7 @@ const Job = ({ job }) => {
     const { id, logo, job_title, company_name, remote_or_onsite, location, job_type, salary } = job;
     return (
         <div className="card card-compact bg-base-100 shadow-xl">
-            <figure><img src={logo} alt="Shoes" /></figure>
+            <figure><img src={logo} alt="Shoes" className="h-[370px] w-[570px]" /></figure>
             <div className="card-body">
                 <h2 className="card-title">{job_title}</h2>
                 <p>{company_name}</p>
